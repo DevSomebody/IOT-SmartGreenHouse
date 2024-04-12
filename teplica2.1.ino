f5b106150659401e0b3478093c20feb17dee6fca
VladislavLogunovDev <116576670+DevSomebody@users.noreply.github.com>
2024-04-12 10:35:05 +0800

    Add files via upload

---
+++ teplica2.1.ino
@@ -0,0 +1,149 @@
+ #include <WiFi.h>
+ #include <PubSubClient.h>
+ #include <Adafruit_Sensor.h>  
+  
+  #include <DHT.h> 
+
+// Указываем пины для подключения датчиков
+#define DHT_PIN 5      // Пин, к которому подключен датчик DHT11
+#define SOIL_PIN 4    // Пин, к которому подключен датчик влажности почвы
+#define LIGHT_PIN 35   // Пин, к которому подключен фоторезистор
+#define FAN_PIN 2   // Пин, к которому подключен кулер
+
+
+
+ /////////////////// SETTINGS ///////////////////////////// 
+   
+  // Wi-Fi 
+  const char* ssid = "Galaxy M31922F"; 
+  const char* password = "123123123"; 
+
+
+  // MQTT 
+  const char* mqtt_server = "m3.wqtt.ru"; 
+  const int mqtt_port = 10273; 
+  const char* mqtt_user = "u_BHWYQV"; 
+  const char* mqtt_password = "71fnLgKU"; 
+
+// Указываем идентификатор устройства и темы MQTT
+  
+  //const int sending_period = 5; 
+const int sending_period = 5; 
+const char* temperature_topic = "test9999/temperature";
+const char* humidity_topic = "test9999/humidity";
+const char* soil_moisture_topic = "test9999/soil_humidity";
+const char* light_intensity_topic = "test9999/light_intensity";
+
+const char* fan_control_topic = "test9999/fan_control";
+const char* led_control_topic = "test9999/led_control";
+
+// Создаем объекты для работы с датчиками
+DHT dht(DHT_PIN, DHT11);
+
+// Создаем объект для работы с Wi-Fi
+WiFiClient espClient;
+PubSubClient client(espClient);
+uint32_t tmr1; 
+// Функция для подключения к Wi-Fi сети
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void reconnect() { 
+    while (!client.connected()) { 
+      Serial.print("Attempting MQTT connection..."); 
+      String clientId = "ESP32-" + WiFi.macAddress(); 
+      if (client.connect(clientId.c_str(), mqtt_user, mqtt_password) ) { 
+        Serial.println("connected"); 
+      client.subscribe(fan_control_topic);
+      client.subscribe(led_control_topic);
+
+      } else { 
+        Serial.print("failed, rc="); 
+        Serial.print(client.state()); 
+        Serial.println(" try again in 5 seconds"); 
+        delay(5000); 
+      } 
+    } 
+  } 
+
+// Функция для отправки данных на MQTT брокер
+  void send_sensor_data() {
+    
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity(); 
+  int soil_moisture = digitalRead(SOIL_PIN);
+  int light_intensity = analogRead(LIGHT_PIN);
+
+  // Отправляем данные о температуре, влажности, влажности почвы и освещенности
+  client.publish(temperature_topic, String(temperature).c_str());
+  client.publish(humidity_topic, String(humidity).c_str());
+  client.publish(soil_moisture_topic, String(soil_moisture).c_str());
+  client.publish(light_intensity_topic, String(light_intensity).c_str());
+  
+//  Serial.println(light_intensity_topic);
+//  Serial.println(soil_moisture_topic);
+  
+  Serial.println(soil_moisture);
+  Serial.println(light_intensity);
+  
+  
+}
+
+// Функция, которая вызывается при получении сообщения от MQTT брокера
+void callback(char* topic, byte* payload, unsigned int length) {
+  
+  String payloadStr = "";
+  for (int i = 0; i < length; i++) {
+    payloadStr += (char)payload[i];
+  }
+
+  // Проверяем условия для управления исполнительными устройствами
+
+  
+  int value = payloadStr.toInt();
+  if (strcmp(topic, humidity_topic) == 0 && value > 80) {
+    digitalWrite(FAN_PIN, HIGH);
+    Serial.println("FAN turned ON due to light condition");
+  } 
+  else {
+    digitalWrite(FAN_PIN, LOW);
+    Serial.println("FAN turned OFF");
+  }
+}  
+
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  dht.begin();
+  pinMode(SOIL_PIN, INPUT);
+  pinMode(LIGHT_PIN, INPUT);
+}
+
+void loop() {
+ if (!client.connected()) { 
+      reconnect(); 
+    } 
+    client.loop(); 
+   
+    if (millis() - tmr1 >= (sending_period * 100)) { 
+      tmr1 = millis(); 
+  send_sensor_data();
+}}
